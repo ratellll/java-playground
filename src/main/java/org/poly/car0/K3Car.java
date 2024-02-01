e2745e1a3c8afbe061b1b16321cd48c2fe47db3a
ratellll <latellbin@naver.com>
2024-02-01 23:17:32 +0900

    feat: 다형성 예제연습

---
+++ src/main/java/org/poly/car0/K3Car.java
@@ -0,0 +1,15 @@
+package org.poly.car0;
+
+public class K3Car {
+    public void startEngine() {
+        System.out.println("K3Car.startEngine");
+    }
+
+    public void endEngine() {
+        System.out.println("K3Car.endEngine");
+    }
+
+    public void pressAccel() {
+        System.out.println("K3Car.pressAccel");
+    }
+}
