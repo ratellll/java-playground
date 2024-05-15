6f869e3c351d9d11d003af2b25a852688aaab9eb
ratellll <latellbin@naver.com>
2024-05-15 17:19:01 +0900

    feat: java

---
+++ src/main/java/org/variable/Var7.java
@@ -0,0 +1,14 @@
+package org.variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; //정수
+        double b =10.5 ; //실수
+        boolean c =true ; //불리언 true , false 입력가능
+        char d = 'A'; //문자하나
+        String e = "빈이"; // 문자열을 다루기위한타입
+        //위와같이 고정된 변수들은 리터럴 이라고 말함
+
+
+    }
+}
