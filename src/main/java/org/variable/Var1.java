7656ac6aea4250dafa437e3c283a6e24d64c34e6
ratellll <latellbin@naver.com>
2024-05-15 16:52:16 +0900

    feat: java

---
+++ src/main/java/org/variable/Var1.java
@@ -0,0 +1,11 @@
+package org.variable;
+
+public class Var1 {
+
+    public static void main(String[] args) {
+        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);
+
+    }
+}
---
+++ src/main/java/org/variable/Var2.java
@@ -0,0 +1,8 @@
+package org.variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int a; //변수 선언 (변수란 데이터를담는통이라고 생각하면 편함)
+                a = 10 ; //변수 초기화하기 , 자바에서 = 는 오른쪽값을 왼쪽에 저장한다는뜻 * equals를 뜻하는게아님
+            }
+}
