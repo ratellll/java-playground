f7b0f4f2f634d803153abbac83f98a98dd83716d
ratellll <latellbin@naver.com>
2024-05-16 16:20:26 +0900

    feat: java

---
+++ src/main/java/org/variable/ex3.java
@@ -0,0 +1,11 @@
+package org.variable;
+
+public class ex3 {
+    public static void main(String[] args) {
+        long l1 = 10000000000L;
+        boolean check = true;
+
+        System.out.println(l1);
+        System.out.println(check);
+    }
+}
