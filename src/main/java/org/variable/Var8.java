03c5bf5ec9cac66ef93820d57bce051572a23766
ratellll <latellbin@naver.com>
2024-05-16 14:54:00 +0900

    feat: java

---
+++ src/main/java/org/variable/Var8.java
@@ -0,0 +1,19 @@
+package org.variable;
+
+public class Var8 {
+
+    public static void main(String[] args) {
+        //정수
+        byte b = 127; // -128 ~ 127
+        short s = 32767; //-32,768 ~ 32,767
+        int i = 2147483647; //- 2,147,483 ,648~
+
+        long l = 9999999L; //매우큼 L을 넣어줘야함
+
+        //실수
+        float f = 10.0f; // 범위가 작아 제대로 계산이안됨  실무에서 잘 안씀     s
+        double d = 10.0;
+
+
+    }
+}
