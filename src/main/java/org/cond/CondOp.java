7ebf90f9d958feb156fdb3be0146fa8c2edfc452
ratellll <latellbin@naver.com>
2024-05-17 18:16:17 +0900

    feat: java

---
+++ src/main/java/org/cond/CondOp.java
@@ -0,0 +1,12 @@
+package org.cond;
+
+public class CondOp {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        String status = (age >= 20) ? "성인" : "민짜";
+
+        System.out.println(status);
+    }
+}
