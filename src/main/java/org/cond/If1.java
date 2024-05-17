3ca2e58688ae8e4a76f1561fe5848b0b5f042923
ratellll <latellbin@naver.com>
2024-05-17 17:21:07 +0900

    feat: java

---
+++ src/main/java/org/cond/If1.java
@@ -0,0 +1,14 @@
+package org.cond;
+
+public class If1 {
+    public static void main(String[] args) {
+
+        int age = 15;
+
+        if (age >= 20) {
+            System.out.println("입장가능");
+        }else {
+            System.out.println("입벤");
+        }
+    }
+}
