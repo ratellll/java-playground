ba13e941837b0790b1bfa44d169a5f5f7c35b4eb
ratellll <latellbin@naver.com>
2024-05-21 18:35:15 +0900

    feat: java

---
+++ src/main/java/org/method/Method4.java
@@ -0,0 +1,21 @@
+package org.method;
+
+public class Method4 {
+    public static void main(String[] args) {
+
+        check(28);
+        check(20);
+        check(23);
+        check(19);
+    }
+
+    public static void check(int age) {
+        if (age >= 20) {
+            System.out.println("입장가능ㅋㅋ");
+            return;
+        }
+            System.out.println("민짜 ㄴㄴ ㅋㅋ");
+        }
+
+
+}
