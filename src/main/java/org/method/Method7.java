e5780a0d5f39bc9c7aab3e3417bd4eeb35ef3e83
ratellll <latellbin@naver.com>
2024-05-23 17:17:52 +0900

    feat: java

---
+++ src/main/java/org/method/Method7.java
@@ -0,0 +1,14 @@
+package org.method;
+
+public class Method7 {
+    public static void main(String[] args) {
+        double num = 1.5;
+
+        printNum((int) num);
+
+    }
+
+    public static void printNum(int num) {
+        System.out.println(num);
+    }
+}
