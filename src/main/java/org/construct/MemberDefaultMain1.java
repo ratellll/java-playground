db3eea13ffdfa697ef017681d3867d71de8f04a2
ratellll <latellbin@naver.com>
2024-05-31 22:41:27 +0900

    feat: java

---
+++ src/main/java/org/construct/MemberDefaultMain1.java
@@ -0,0 +1,7 @@
+package org.construct;
+
+public class MemberDefaultMain1 {
+    public static void main(String[] args) {
+        MemberDefault memberDefault = new MemberDefault(); // 생성자가 없기때문에 기본 생성자를 만들어줌.
+    }
+}
