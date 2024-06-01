389f819b031b1b00bbeee4c53f461eb5945dffd6
ratellll <latellbin@naver.com>
2024-06-01 17:01:22 +0900

    feat: java

--- src/main/java/org/access/Speaker.java
+++ src/main/java/org/access/Speaker.java
@@ -1,36 +1,33 @@
 package org.access;
 
 public class Speaker {
-
-    private int volume;
-
+     private int volume = 50;
 
     Speaker(int volume) {
         this.volume = volume;
     }
 
-
     void volumeUp() {
         if (volume >= 100) {
-            System.out.println("최대 음량입니다.");
-        } else {
+            System.out.println("음량을 증가할수없습니다 , 최대 음량입니다");
+        }else {
             volume += 10;
             System.out.println("음량을 10 증가합니다");
-            System.out.println("현재 음량은 " + volume +" 입니다.");
+            System.out.println("현재 음량 = " + volume);
         }
-   }
+    }
 
-   void volumeDown() {
-       if (volume == 0) {
-           System.out.println("최소 음량 입니다");
-       } else {
-           volume -= 10;
-           System.out.println("음량을 10 감소합니다");
-           System.out.println("현재 음량은 " + volume +" 입니다.");
-       }
-       }
+    void volumeDown(){
+        if (volume == 0) {
+            System.out.println("볼륨이 0 입니다. 최저 음량입니다.");
+        }else {
+            volume -= 10;
+            System.out.println("음량을 10 감소합니다");
+            System.out.println("현재 음량 = " + volume);
+        }
+    }
 
     void showVolume() {
-        System.out.println("현재 음량은 " + volume +" 입니다.");
+        System.out.println("현재 음량 = " + volume);
     }
 }
--- src/main/java/org/access/SpeakerMain.java
+++ src/main/java/org/access/SpeakerMain.java
@@ -2,21 +2,39 @@ package org.access;
 
 public class SpeakerMain {
     public static void main(String[] args) {
-        Speaker speaker = new Speaker(50);
-        speaker.showVolume();
-        speaker.volumeUp();
-        speaker.volumeUp();
-        speaker.volumeUp();
-        speaker.volumeUp();
-        speaker.volumeUp();
-        speaker.volumeUp();
-        speaker.volumeDown();
-        speaker.volumeDown();
-        speaker.volumeDown();
-        speaker.volumeDown();
-        speaker.volumeDown();
+        Speaker sp = new Speaker(80);
 
+        System.out.println("볼륨 2번 업");
+        sp.volumeUp();
+        sp.volumeUp();
 
+        System.out.println("볼륨 11번 다운 , 경고메세지 확인 ");
+        sp.volumeDown();
+        sp.volumeDown();
+        sp.volumeDown();
+        sp.volumeDown();
+        sp.volumeDown();
+        sp.volumeDown();
+        sp.volumeDown();
+        sp.volumeDown();
+        sp.volumeDown();
+        sp.volumeDown();
+        sp.volumeDown();
 
+        System.out.println("볼륨 11번 업, 경고메세지 확인");
+        sp.volumeUp();
+        sp.volumeUp();
+        sp.volumeUp();
+        sp.volumeUp();
+        sp.volumeUp();
+        sp.volumeUp();
+        sp.volumeUp();
+        sp.volumeUp();
+        sp.volumeUp();
+        sp.volumeUp();
+        sp.volumeUp();
+
+        System.out.println("볼륨 확인");
+        sp.showVolume();
     }
 }
