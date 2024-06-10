88495e8c9a276a743627fb999d568bde7905723d
ratellll <latellbin@naver.com>
2024-06-10 16:41:03 +0900

    feat: java

---
+++ src/main/java/org/poly/Car0/K3Car.java
@@ -0,0 +1,16 @@
+package org.poly.Car0;
+
+public class K3Car {
+
+    public void startEngine() {
+        System.out.println("K3Car.startEngine");
+    }
+
+    public void offEngine() {
+        System.out.println("K3Car.offEngine");
+    }
+
+    public void Accelerator() {
+        System.out.println("K3Car.Accelrator");
+    }
+}
