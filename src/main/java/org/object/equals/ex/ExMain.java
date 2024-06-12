3d6b2b43ad8d606ac78899dad9a9581843df643a
ratellll <latellbin@naver.com>
2024-06-12 17:33:24 +0900

    feat: java

---
+++ src/main/java/org/object/equals/ex/ExMain.java
@@ -0,0 +1,15 @@
+package org.object.equals.ex;
+
+public class ExMain {
+    public static void main(String[] args) {
+
+        Rectangle rec1 = new Rectangle(100, 20);
+        Rectangle rec2 = new Rectangle(100, 20);
+
+
+        System.out.println(rec1);
+        System.out.println(rec2);
+        System.out.println(rec1 == rec2);
+        System.out.println(rec1.equals(rec2));
+    }
+}
