4c65c3dfdbdd762db2bced5e43c756dd96a0c7d5
ratellll <latellbin@naver.com>
2024-06-12 18:01:06 +0900

    feat: java

---
+++ src/main/java/org/immutable/address/PrimitiveMain.java
@@ -0,0 +1,16 @@
+package org.immutable.address;
+
+public class PrimitiveMain {
+    public static void main(String[] args) {
+        //기본형은 절대로 같은 값을 공유하지 않는다.
+        int a = 10;
+        int b = a; // a ->b 값 복사 후 대입
+
+        System.out.println("A = " +a);
+        System.out.println("B = " +b);
+
+        b = 20;
+        System.out.println("A = " +a);
+        System.out.println("B = " +b);
+    }
+}
