7d22ec10b56ca2d05138876123d470299dbe2dd8
ratellll <latellbin@naver.com>
2024-06-18 17:30:43 +0900

    feat: java

---
+++ src/main/java/org/string/im/StringImMain.java
@@ -0,0 +1,11 @@
+package org.string.im;
+
+public class StringImMain {
+    public static void main(String[] args) {
+
+        String str = "hi";
+        String concat = str.concat("2");
+
+        System.out.println(concat);
+    }
+}
